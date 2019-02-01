c5adbdba3ae8113d57f1d04a413d32e884897ef8
Giv Parvaneh <giv@givp.org>
2019-01-31 17:36:17 -0800

    Added script parser and view

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,43 +1,59 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
+      <div>Import your script (Final Draft .fdx file)</div>
+      <div><button @click="openFile">Open</button></div>
+
+      <div class="scriptContent" v-if="content">
+        <div v-for="(item, index) in content">
+          <div v-bind:class="item.$ && item.$.Type.replace(/ +/g, '')">{{ item.Text && item.Text[0] }}</div>
+        </div>
       </div>
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
     </main>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
+  const { dialog } = require('electron').remote;
+  var fs = require('fs');
+  var parseString = require('xml2js').parseString;
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    data () {
+      return {
+        debugText: "",
+        content: null
+      }
+    },
+    mounted: function () {
+      //this.parseFile(["/Users/giv/Desktop/dig.fdx"])
+    },
     methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+      openFile() {
+        var vm = this
+        dialog.showOpenDialog({ properties: ['openFile'], filters: [
+          { name: 'Final Draft', extensions: ['fdx'] }
+        ]
+        }, this.parseFile)
+      },
+      parseFile(paths) {
+        var vm = this
+
+        var xmlPath = paths[0]
+        console.log(xmlPath)
+        fs.readFile(xmlPath, 'utf-8', (err, data) => {
+          if(err){
+              alert("An error ocurred reading the file :" + err.message);
+              return;
+          }
+
+          parseString(data, function (err, result) {
+            vm.content = result.FinalDraft.Content[0].Paragraph
+            console.log(vm.content)
+          });
+        });
       }
     }
   }
@@ -52,77 +68,48 @@
     padding: 0;
   }
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: #e6e6e6;
+    font-color: #333;
+    padding: 50px;
+  }
 
   #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
   }
-
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
   main > div { flex-basis: 50%; }
 
-  .left-side {
-    display: flex;
-    flex-direction: column;
+  .scriptContent {
+    font-family: "CourierPrime", monospace;
+    font-size: 15px;
+    margin-top: 20px;
+    font-weight: 500;
+    background: #fff;
+    width: 750px;
+    padding: 50px;
   }
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
+  .SceneHeading {
+    margin-top: 40px;
     font-weight: bold;
-    margin-bottom: 6px;
   }
 
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
+  .Action {
+    margin-top: 25px;
   }
 
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
+  .Character {
+    text-align: center;
+    margin-top: 20px;
   }
 
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
+  .Dialogue {
+    text-align: center;
   }
 
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+  .Parenthetical {
+    text-align: center;
+    font-style: italic;
   }
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,10 +4,16 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import db from './datastore'
+import VueLodash from 'vue-lodash'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+Vue.prototype.$db = db
+
+const options = { name: 'lodash' }
+Vue.use(VueLodash, options)
 
 /* eslint-disable no-new */
 new Vue({
